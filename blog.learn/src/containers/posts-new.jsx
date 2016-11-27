01af0b3e33233d2d32de05f60c665526a37c1f00
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-11-27 15:13:34 +0800

    Removed line which imports redux's bindActionCreators

--- blog.learn/src/containers/posts-new.jsx
+++ blog.learn/src/containers/posts-new.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux'; 
-import { bindActionCreators } from 'redux';
 import { reduxForm, Field } from 'redux-form';
 import { createPost } from '../actions/create-post.action';
 
