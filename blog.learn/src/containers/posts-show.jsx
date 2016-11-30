7ad30bd51817f17eed672f973a0bd0ea2d8b836b
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-01 05:55:30 +0800

    Added delete button on post show and call the delete post action when the button is clicked.

--- blog.learn/src/containers/posts-show.jsx
+++ blog.learn/src/containers/posts-show.jsx
@@ -1,16 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component,PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
-import { FetchPost } from '../actions';
+import { deletePost, FetchPost } from '../actions';
 
 class PostsShow extends Component {    
+    static contextTypes = {
+        router: PropTypes.object
+    };
 
     componentWillMount() {        
         const id = this.props.params.id;
         console.log('[DEBUG-PostsShow] - id = ', id);
         this.props.FetchPost(id);
     }
-        
+
+    onDeletingPost(){
+        const id = this.props.params.id;
+        this.props.deletePost(id)
+            .then(this.context.router.push('/'));
+    }
+
     render() {
         const post = this.props.post;
         console.log('[DEBUG-PostsShow] - this.props.post=', post);
@@ -20,6 +29,9 @@ class PostsShow extends Component {
 
         return (
             <div>
+                <Link to="/">Back to Index</Link>
+                <button className="btn btn-danger pull-right" 
+                        onClick={this.onDeletingPost.bind(this)}>Delete Post</button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -34,4 +46,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps, { FetchPost })(PostsShow);
+export default connect(mapStateToProps, { deletePost, FetchPost })(PostsShow);
