997b9a5e4765965f09761e64a396e4152272b56b
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-04 15:46:26 +0800

    Fix issue in render method which was unable rendering post items list.

--- blog/src/containers/posts-index.jsx
+++ blog/src/containers/posts-index.jsx
@@ -10,21 +10,29 @@ class PostsIndex extends Component {
 
     render () {
         const posts = this.props.posts;
+        //const posts = [ {id: '123', title: 'test', categories: 'general'}, { id: '345', title: 'test2', categories: 'general'} ];
+        const noItemsInfo = `You have no any post items yet. Click Add Post to add your 1st post.`;
         return (
             <div className="posts-index">
                 <div className="text-right"><button className="btn btn-primary">Add Post</button></div>
                 <hr />
-                { posts.length > 0 ? posts.map( post => this.renderPostItem(post)) :
-                                     <span className="alert alert-info">'You have no any post items yet. Click Add Post to add your 1st post.'</span> }
+                <div>
+                    {posts.length > 0 ? 
+                        <ul className="list-group"> {posts.map(post => this.renderPostItem(post))}</ul> : 
+                        <span className="alert alert-info">{noItemsInfo}</span> }
+                                         
+                </div>
             </div>
         )
     }
 
     renderPostItem(post){
-        <li className="lsit-group-item">
-            <span>{post.title}</span>
-            <span className="badge">{post.categories}</span>
-        </li>
+        return (
+            <li className="list-group-item" key={post.id}>
+                <span>{post.title}</span>
+                <span className="badge">{post.categories}</span>
+            </li>
+        )
     }
 }
 
