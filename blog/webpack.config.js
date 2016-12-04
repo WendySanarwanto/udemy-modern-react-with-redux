4977cd5453c7fb8d895e37e9c84f57e8c652e9b1
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-04 08:35:44 +0800

    Initial commit with required dependencies in package.json & proper README.md file.

---
+++ blog/webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+  },
+  devServer: {
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    contentBase: './'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin({multiStep: true})
+  ]
+};
