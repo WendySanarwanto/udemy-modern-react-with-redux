ed8e5ae1f1d104847b6cf92c7686f17dd3c143c4
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-04 10:22:18 +0800

    Added Add Post button on PostsIndex component.

--- blog/src/components/posts-index.jsx
+++ blog/src/components/posts-index.jsx
@@ -4,7 +4,7 @@ class PostsIndex extends Component {
     render () {
         return (
             <div className="posts-index">
-                <div>TODO: Display Add Post button</div>
+                <div className="text-right"><button className="btn btn-primary">Add Post</button></div>
                 <div>TODO: Display a list of clickable Post Items</div>
             </div>
         )
