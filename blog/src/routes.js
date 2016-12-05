740aedaf4758c617246f9c52606313a412b357cd
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-06 06:52:11 +0800

    Added a new route for navigating to Add New Post page.

--- blog/src/routes.js
+++ blog/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import { App } from './components';
-import { PostsIndex } from './containers';
+import { PostsIndex, PostsNew } from './containers';
 
 /** 
  * Export component which render <Route> and its children. These markups define the page navigation routes within our application
@@ -10,6 +10,7 @@ import { PostsIndex } from './containers';
 export default (    
     <Route path="/" component={App}>
         <IndexRoute component={PostsIndex}/>
-        <Route path="posts" component={PostsIndex} />    
+        <Route path="posts" component={PostsIndex} />
+        <Route path="posts/new" component={PostsNew} />            
     </Route>
 );
