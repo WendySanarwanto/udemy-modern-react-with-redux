aa8c708dfdfaea602f5d95e27514f5ca43274f84
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-06 20:51:21 +0800

    Refactor postsnew component by rebuilding the form using redux-form

--- blog/src/containers/posts-new.jsx
+++ blog/src/containers/posts-new.jsx
@@ -1,23 +1,16 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router';
+import { Field, reduxForm } from 'redux-form';
 
-class PostsNew extends Component {
+class PostsNew extends Component {    
     render() {
+        const { handleSubmit } = this.props;
         return (
             <div className="posts-new container">
-                <form className="post-new-form">
-                    <div className="form-group">
-                        <label htmlFor="title">Title</label>
-                        <input type="text" className="form-control" id="title" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="categories">Categories</label>
-                        <input type="text" className="form-control" id="categories" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="contents">Contents</label>
-                        <textarea rows="10" cols="40" className="form-control" id="contents" />
-                    </div>
+                <form onSubmit={handleSubmit(this.onSubmit)} className="post-new-form">
+                    <Field name="title" label="title" type="text" component={this.renderInputField}/>
+                    <Field name="categories" label="categories" type="text" component={this.renderInputField}/>
+                    <Field name="contents" rows="10" cols="40" label="contents" type="textarea" component={this.renderTextAreaField}/>
                     <button type="submit" className="btn btn-primary">&nbsp;Save&nbsp;</button>
                     <Link type="button" to="/" className="btn btn-danger post-new-form-cancel-btn">
                         Cancel
@@ -26,6 +19,30 @@ class PostsNew extends Component {
             </div>
         );
     }
+
+    renderInputField({ input, label, name, type }){
+        return (
+            <div className="form-group">
+                <label htmlFor={name}>{label}</label>
+                <input type={type} className="form-control" id={name} {...input} />
+            </div>
+        );    
+    }
+
+    renderTextAreaField({ input, rows, cols, label, name, type }){
+        return (
+            <div className="form-group">
+                <label htmlFor={name}>{label}</label>
+                <textarea rows={rows} cols={cols} className="form-control" id={name} {...input} />
+            </div>
+        );    
+    }
+    
+    onSubmit(values){
+        console.log('[DEBUG-PostsNew] - onSubmit is called. values=', values);
+    }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'createNewPost'
+})(PostsNew);
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import PostsReducer from './posts.reducer';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
