44d224909758a610d1ba1c03d125ac666bab5b9c
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-10 07:28:06 +0800

    Added createPost action creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -4,12 +4,22 @@ const API_KEY = '?key=5220dec8-15d0-4618-904e-0a56466afde9';
 const API_BASE_URL = 'http://reduxblog.herokuapp.com/api/posts';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 export function fetchPosts(){
     const apiUrl = `${API_BASE_URL}${API_KEY}`;
-    let request = axios.get(apiUrl);
+    const request = axios.get(apiUrl);
     return {
         type: FETCH_POSTS,
         payload: request
     };
+}
+
+export function createPost(newPost){
+    const apiUrl = `${API_BASE_URL}${API_KEY}`;
+    const request = axios.post(apiUrl, newPost);
+    return {
+        type: CREATE_POST,
+        payload: request
+    }
 }
