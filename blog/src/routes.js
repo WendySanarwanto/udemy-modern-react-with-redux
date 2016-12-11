58a9d06708bdc7b4c4645032f3f887a09e0873a9
WendySanarwanto <wendy.sanarwanto@gmail.com>
2016-12-11 11:57:30 +0800

    Added route for navigating Post View page.

--- blog/src/routes.js
+++ blog/src/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import { App } from './components';
-import { PostsIndex, PostsNew } from './containers';
+import { PostsIndex, PostsNew, PostsView } from './containers';
 
 /** 
  * Export component which render <Route> and its children. 
@@ -12,6 +12,7 @@ export default (
     <Route path="/" component={App}>
         <IndexRoute component={PostsIndex}/>
         <Route path="posts" component={PostsIndex} />
-        <Route path="posts/new" component={PostsNew} />            
+        <Route path="posts/new" component={PostsNew} /> 
+        <Route path="posts/:id" component={PostsView}></Route>           
     </Route>
 );
