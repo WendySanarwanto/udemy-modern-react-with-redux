7fd6c898cacab496c935f7e44d5a29093f57c14c
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-10-10 03:58:24 +0800

    Removed failing project

--- create-delete-firebase/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import dataListReducer from './reducer_datalist';
-
-const rootReducer = combineReducers({
-  // state: (state = {}) => state
-  dataList: dataListReducer
-});
-
-export default rootReducer;
