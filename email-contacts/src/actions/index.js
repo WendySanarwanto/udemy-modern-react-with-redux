6c4c6496a9318fb3ffe633b989533990ceaa9064
WendySanarwanto <wendy.sanarwanto@gmail.com>
2018-10-12 18:27:48 +0800

    Do some cleanups

--- email-contacts/src/actions/index.js
+++ email-contacts/src/actions/index.js
@@ -5,9 +5,9 @@ export const FETCH_DATA = 'fetch_data';
 const API_URL = 'http://jsonplaceholder.typicode.com/users';
 
 export function fetchData() {
-    // TODO: Implement this
     const request = axios.get(API_URL);
 
+    // Handle dispatch manually 
     return (dispatch) => {
         request.then(({data}) => {
             dispatch({
--- reselect-demo/src/actions/index.js
+++ reselect-demo/src/actions/index.js
@@ -1,15 +1,15 @@
 export const FETCH_POSTS = 'fetch_posts';
 export const SELECT_POST = 'select_post';
 
-export function fetch_posts() {
-  return {
-    type: FETCH_POSTS
-  }
+export function fetchPosts(){
+	return {
+		type: FETCH_POSTS
+	};
 }
 
-export function select_post(id) {
-  return {
-    type: SELECT_POST,
-    payload: id
-  }
+export function selectPost(id, isSelected){
+	return {
+		type: SELECT_POST,
+		payload: { id, isSelected}
+	}
 }
