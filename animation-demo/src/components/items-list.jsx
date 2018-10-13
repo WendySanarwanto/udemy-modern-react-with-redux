76fa74e79f3cbaa55a8d14b7dca634025bdb7db8
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2018-10-13 08:39:59 +0800

    Added css animation

--- animation-demo/src/components/items-list.jsx
+++ animation-demo/src/components/items-list.jsx
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
 import { removeItem } from '../actions';
 import './items-list.css';
@@ -12,28 +13,37 @@ class ItemsList extends Component {
     this.props.removeItem(item);
   }
 
+  renderItems(items) {
+    // console.log(`[DEBUG] - <ItemsList.renderItems> items: \n`, items);
+    return (
+      _.map(items, item => {
+        return (
+          <li className="list-group-item" key={item.id}>
+            {item.title}
+            <button className="remove btn btn-danger" onClick={()=>this.onRemoveItem(item)}>Remove</button>
+          </li>
+        );
+      })
+    );
+  }
+
   render() {
     const { items } = this.props;
-    // console.log(`[DEBUG] - <ItemsList.render> items: \n`, items);
-
+    const transitionOptions = {
+      transitionName: "fade",
+      transitionEnterTimeout: 500,
+      transitionLeaveTimeout: 500
+    };    
     return (
       <div className="items-list-container">
         <h4>Items list</h4>
-        { items && items.length > 0 ?
-            <ul className="list-group">
-              {
-                _.map(items, item => {
-                  return (
-                    <li className="list-group-item" key={item.id}>
-                      {item.title}
-                      <button className="remove btn btn-danger" onClick={()=>this.onRemoveItem(item)}>Remove</button>
-                    </li>
-                  );
-                })
-              }
-            </ul> : 
-            <span>Please click the Add button to add items in here.</span>
-        }
+          {!items || _.size(items) < 1 ? <span>Please click the Add button to add items in here.</span> : ''}
+          <ul className="list-group">
+            <ReactCSSTransitionGroup {...transitionOptions}>
+            {items && _.size(items) > 0 ? this.renderItems(items)  : ''}
+            </ReactCSSTransitionGroup>
+          </ul>
+
       </div>
     );
   }
