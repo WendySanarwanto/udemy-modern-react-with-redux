d08145f4a03934da4a8845e03e53f207dfe468d0
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-01-07 10:08:45 +0800

    Refactor App Component to be a functional component

--- weather/src/components/app.js
+++ weather/src/components/app.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SearchBar from '../containers/search_bar';
 import WeatherList from '../containers/weather_list';
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <SearchBar />
-        <WeatherList />
-      </div>
-    );
-  }
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+      <WeatherList />
+    </div>
+  );
 }
+
+export default App;
